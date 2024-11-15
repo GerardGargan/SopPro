b6510ffae9947311bc612d990079f57c8edb8dac
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-15 00:27:29 +0000

    Configure db context and add application user and migrations

--- Backend/Backend/Data/ApplicationDbContext.cs
+++ Backend/Backend/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
