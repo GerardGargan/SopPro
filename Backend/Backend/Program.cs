b463a9a3a14fd98aa4d6b5dde7ef5512019dccff
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-15 00:03:28 +0000

    Set up db context and identity framework

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,7 +1,16 @@
+using Backend.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
