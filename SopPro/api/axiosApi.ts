c706e83171c30dc23acf5654c6a1cf86b426f52a
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-30 23:13:12 +0000

    Add interceptor to send authorisation token if its in the store

--- SopPro/api/axiosApi.ts
+++ SopPro/api/axiosApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import store from '../store/index';
 
 // TODO: Set up a environment variable for baseURL
 const api = axios.create({
@@ -8,17 +9,31 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use(
+  (config) => {
+  const token = store.getState().auth.token;
+
+  if(token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+},
+(error) => {
+  return Promise.reject(error);
+});
+ // interceptor for loggout out response data to the console for inspection
 api.interceptors.response.use(
   (response) => {
-    console.log("Response Data:", response.data);
+    console.log("Response Data:", JSON.stringify(response.data, null, 2));
     return response;
   },
   (error) => {
     if (error.response) {
       // Server responded with a status other than 2xx
-      console.error("Response Error Data:", error.response.data);
+      console.error("Response Error Data:", JSON.stringify(error.response.data, null, 2));
       console.error("Response Error Status:", error.response.status);
-      console.error("Response Error Headers:", error.response.headers);
+      console.error("Response Error Headers:", JSON.stringify(error.response.headers, null, 2));
     } else if (error.request) {
       // No response received
       console.error("No Response Received:", error.request);
@@ -26,6 +41,26 @@ api.interceptors.response.use(
       // Something went wrong in setting up the request
       console.error("Error Config:", error.message);
     }
+    return Promise.reject(error);
+  }
+);
+
+// interceptor for logging out request data to the console for inspection
+api.interceptors.request.use(
+  (config) => {
+    // Log the full request details
+    console.log("API Request:");
+    console.log("URL:", config.url);
+    console.log("Method:", config.method);
+    console.log("Headers:", config.headers);
+    if (config.data) {
+      console.log("Data:", JSON.stringify(config.data, null, 2)); 
+    }
+
+    return config;
+  },
+  (error) => {
+    console.error("Request Error:", error);
     return Promise.reject(error);
   }
 );
