e0e862f5b5482241d13d169880a442335f9c77cb
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-12-28 12:28:36 +0000

    Update test endpoint

--- Backend/Backend/Controllers/WeatherForecastController.cs
+++ Backend/Backend/Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
+using Backend.Data;
 using Backend.Models.DatabaseModels;
 using Backend.Models.Settings;
+using Backend.Models.Tenancy;
 using Backend.Repository.Interface;
 using Backend.Service.Interface;
 using Microsoft.AspNetCore.Authorization;
@@ -24,18 +26,22 @@ namespace Backend.Controllers
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IEmailService _emailService;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ITenancyResolver _tenancyResolver;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptions<ApplicationSettings> appSettings, IUnitOfWork unitOfWork, IEmailService emailService)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptions<ApplicationSettings> appSettings, IUnitOfWork unitOfWork, IEmailService emailService, ApplicationDbContext dbContext, ITenancyResolver tenancyResolver)
         {
             _logger = logger;
             _appSettings = appSettings.Value;
             _unitOfWork = unitOfWork;
             _emailService = emailService;
+            _dbContext = dbContext;
+            _tenancyResolver = tenancyResolver;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         //[Authorize]
-        public async Task<IEnumerable<ApplicationUser>> Get()
+        public async Task<IEnumerable<Department>> Get()
         {
             //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             //{
@@ -50,10 +56,11 @@ namespace Backend.Controllers
             //_unitOfWork.ApplicationUser.Remove(userToRemove);
             //await _unitOfWork.SaveAsync();
             //await _emailService.SendEmailAsync("ggargan01@qub.ac.uk", "Test api email", "Testing 123");
+            var tenantId = _tenancyResolver.GetOrganisationid();
             List<string> emails = new List<string>();
             emails.Add("ggargan01@qub.ac.uk");
             //await _emailService.SendEmailAsync(null, emails, "Test", "Testing");
-            return await _unitOfWork.ApplicationUsers.GetAll().ToListAsync();
+            return await _unitOfWork.Departments.GetAll().ToListAsync();
 
         }
     }
