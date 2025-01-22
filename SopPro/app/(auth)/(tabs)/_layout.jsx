bf71721eca7003590bb7e1d727f79f234056c5d8
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-01-22 21:26:01 +0000

    Add bottom sheet functionality and test

--- SopPro/app/(auth)/(tabs)/_layout.jsx
+++ SopPro/app/(auth)/(tabs)/_layout.jsx
@@ -1,8 +1,16 @@
-import { Tabs } from "expo-router";
-import React from "react";
+import { Tabs, useFocusEffect } from "expo-router";
+import React, { useCallback, useRef, useState } from "react";
 import { FontAwesome5, FontAwesome6 } from "@expo/vector-icons";
 import SafeAreaHeader from "../../../components/UI/SareAreaHeader";
-import { useTheme } from "react-native-paper";
+import { Button, useTheme } from "react-native-paper";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import {
+  BackHandler,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 function TabBarIcon({ ...props }) {
   return <FontAwesome5 size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -10,9 +18,28 @@ function TabBarIcon({ ...props }) {
 
 const _layout = () => {
   const theme = useTheme();
+  const bottomSheetRef = useRef(null);
+
+  const [isSheetOpen, setIsSheetOpen] = useState(true);
+
+  const handleSheetChanges = useCallback((index) => {
+    setIsSheetOpen(index >= 0);
+  }, []);
+
+  const closeBottomSheet = () => {
+    bottomSheetRef.current?.close();
+    setIsSheetOpen(false);
+  };
+
+  const openBottomSheet = () => {
+    bottomSheetRef.current?.expand();
+    setIsSheetOpen(true);
+  };
+
   return (
     <>
       <SafeAreaHeader />
+      <Button onPress={openBottomSheet}>Open</Button>
       <Tabs
         screenOptions={{
           headerShown: false,
@@ -50,8 +77,43 @@ const _layout = () => {
           }}
         />
       </Tabs>
+
+      {isSheetOpen && (
+        <TouchableWithoutFeedback onPress={closeBottomSheet}>
+          <View style={styles.overlay} />
+        </TouchableWithoutFeedback>
+      )}
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={isSheetOpen ? 0 : -1}
+        snapPoints={["50%"]}
+        onChange={handleSheetChanges}
+        enablePanDownToClose={true}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+          <Button onPress={closeBottomSheet}>Close</Button>
+        </BottomSheetView>
+      </BottomSheet>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: "center",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+});
 
 export default _layout;
--- SopPro/app/_layout.jsx
+++ SopPro/app/_layout.jsx
@@ -5,6 +5,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-native-reanimated";
 import { Provider } from "react-redux";
@@ -39,16 +40,18 @@ export default function RootLayout() {
   }
 
   return (
-    <SafeAreaProvider>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <PaperProvider theme={customLightTheme}>
-            <RootLayoutNav />
-            <Toast />
-          </PaperProvider>
-        </QueryClientProvider>
-      </Provider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <QueryClientProvider client={queryClient}>
+            <PaperProvider theme={customLightTheme}>
+              <RootLayoutNav />
+              <Toast />
+            </PaperProvider>
+          </QueryClientProvider>
+        </Provider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
