393af68964c2595c933daa7ed8f56a46ccbabe1e
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-01-23 21:00:35 +0000

    Implement custom bottom sheet modal

--- SopPro/app/(auth)/(tabs)/index.jsx
+++ SopPro/app/(auth)/(tabs)/index.jsx
@@ -1,28 +1,29 @@
 import { Text, View } from "react-native";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Fab from "../../../components/sops/fab";
 import { useIsFocused } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 import { Button } from "react-native-paper";
 import Toast from "react-native-toast-message";
+import CustomBottomSheetModal from "../../../components/UI/CustomBottomSheetModal";
 
 const index = () => {
   const isFocused = useIsFocused();
   const router = useRouter();
-  const onPress = () => {
-    Toast.show({
-      type: "success",
-      text1: "Button pressed",
-      visibilityTime: 3000,
-    });
-  };
+  const bottomSheetModalRef = useRef(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
   return (
     <>
       <View style={{ justifyContent: "center", alignItems: "center" }}>
         <Text>Home</Text>
         <Text>Page content to be added...</Text>
-        <Button onPress={onPress}>Press me</Button>
+        <Button onPress={handlePresentModalPress}>Open bottom sheet</Button>
+        <CustomBottomSheetModal ref={bottomSheetModalRef} />
       </View>
       {isFocused && <Fab />}
     </>
---
+++ SopPro/components/UI/CustomBottomSheetModal.jsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { forwardRef, useCallback, useMemo } from "react";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+
+const CustomBottomSheetModal = forwardRef((props, ref) => {
+  const snapPoints = useMemo(() => ["60%"], []);
+
+  const renderBackdrop = useCallback(
+    (props) => (
+      <BottomSheetBackdrop
+        {...props}
+        onPress={() => ref.current?.close()}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+
+  return (
+    <BottomSheetModal
+      ref={ref}
+      index={0}
+      snapPoints={snapPoints}
+      enablePanDownToClose={true}
+      backdropComponent={renderBackdrop}
+    >
+      <BottomSheetView style={styles.contentContainer}>
+        <Text>Awesome 🎉</Text>
+      </BottomSheetView>
+    </BottomSheetModal>
+  );
+});
+
+export default CustomBottomSheetModal;
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  containerHeadline: {
+    fontSize: 24,
+    fontWeight: 600,
+    padding: 20,
+  },
+});
