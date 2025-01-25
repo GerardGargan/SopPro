bfb7017aca4342e5978281d5a1931ed9571d6894
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-01-25 13:15:36 +0000

    Remove test code on index

--- SopPro/app/(auth)/(tabs)/index.jsx
+++ SopPro/app/(auth)/(tabs)/index.jsx
@@ -1,27 +1,16 @@
 import { Text, View } from "react-native";
-import React, { useCallback, useRef } from "react";
 import Fab from "../../../components/sops/fab";
 import { useIsFocused } from "@react-navigation/native";
-import { useRouter } from "expo-router";
 import { Button } from "react-native-paper";
-import CustomBottomSheetModal from "../../../components/sops/bottomSheet/CustomBottomSheetModal";
 
 const index = () => {
   const isFocused = useIsFocused();
-  const router = useRouter();
-  const bottomSheetModalRef = useRef(null);
-
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
 
   return (
     <>
       <View style={{ justifyContent: "center", alignItems: "center" }}>
         <Text>Home</Text>
         <Text>Page content to be added...</Text>
-        <Button onPress={handlePresentModalPress}>Open bottom sheet</Button>
-        <CustomBottomSheetModal ref={bottomSheetModalRef} />
       </View>
       {isFocused && <Fab />}
     </>
