76c2f10fcb776390e6b5ea3469d180ca801717ae
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-01-28 21:28:47 +0000

    Implement pdf service

---
+++ Backend/Backend/Service/Interface/IPdfService.cs
@@ -0,0 +1,9 @@
+using Backend.Models.Dto;
+
+namespace Backend.Service.Interface
+{
+    public interface IPdfService
+    {
+        public byte[] GeneratePdf(string templateName, SopDto model);
+    }
+}
