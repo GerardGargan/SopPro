5c8216f6bf3b817ee592799c67e5ccbd5de96c77
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-02-02 23:07:30 +0000

    Add option for log out and change password

--- SopPro/app/(auth)/(tabs)/more.jsx
+++ SopPro/app/(auth)/(tabs)/more.jsx
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import MenuCard from "../../../components/UI/MenuCard";
+import { KeyRound, LogOut } from "lucide-react-native";
 
 const More = () => {
+  const router = useRouter();
+
+  const menuOptions = [
+    {
+      key: 1,
+      icon: LogOut,
+      title: "Log out",
+      onPress: () => {
+        router.replace("/logout");
+      },
+    },
+    {
+      key: 2,
+      icon: KeyRound,
+      title: "Change password",
+      onPress: () => {
+        router.push("(auth)/user/changePassword");
+      },
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Link href="logout">Logout</Link>
-    </View>
+    <ScrollView style={styles.container}>
+      {menuOptions.map((option) => {
+        return (
+          <MenuCard
+            key={option.key}
+            title={option.title}
+            Icon={option.icon}
+            onPress={option.onPress}
+          />
+        );
+      })}
+    </ScrollView>
   );
 };
 
@@ -15,7 +47,5 @@ export default More;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    paddingTop: 50,
   },
 });
