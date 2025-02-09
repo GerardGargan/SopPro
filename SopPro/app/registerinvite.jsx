b11473e0ca12531c26aa893abd3c92a1dc0c7b16
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-02-09 23:38:54 +0000

    Remove unused imports

--- SopPro/app/registerinvite.jsx
+++ SopPro/app/registerinvite.jsx
@@ -3,8 +3,6 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Text,
-  View,
 } from "react-native";
 import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
