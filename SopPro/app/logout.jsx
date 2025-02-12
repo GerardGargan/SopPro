9b563d0b4a3dbca269972af662629aad46540aff
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-02-12 11:23:56 +0000

    Clear cached data on logout to force refetch when changing user

--- SopPro/app/logout.jsx
+++ SopPro/app/logout.jsx
@@ -1,23 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-import { useEffect } from 'react'
-import { authActions } from '../store/authSlice';
-import { useDispatch } from 'react-redux';
-import { Redirect } from 'expo-router';
+import { useEffect } from "react";
+import { authActions } from "../store/authSlice";
+import { useDispatch } from "react-redux";
+import { Redirect } from "expo-router";
+import { useQueryClient } from "@tanstack/react-query";
 
 const logout = () => {
-
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(authActions.logout());
-    }, [dispatch])
+  const dispatch = useDispatch();
+  const query = useQueryClient();
+  useEffect(() => {
+    query.clear();
+    dispatch(authActions.logout());
+  }, [dispatch, query]);
   return (
     <View>
       <Text>Logging out...</Text>
       <Redirect href="/home" />
     </View>
-  )
-}
+  );
+};
 
-export default logout
+export default logout;
