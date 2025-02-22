e3cde6cdbf53e1f54a16418f17deaa0a167f26c8
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-02-22 18:44:18 +0000

    Handle success and errors, close modal and invalidate queries/cached data

--- SopPro/components/sops/modals/RevertModal.jsx
+++ SopPro/components/sops/modals/RevertModal.jsx
@@ -2,14 +2,33 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import VersionPickerModal from "./VersionPickerModal";
 import useDownload from "../../../hooks/useDownload";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { revertSopVersion } from "../../../util/httpRequests";
+import { QueryClient } from "@tanstack/react-query";
+import Toast from "react-native-toast-message";
 
 const RevertModal = ({ sopVersions, visible, setVisibility }) => {
+  const queryClient = useQueryClient();
   const { mutate, isSuccess, isPending, isError } = useMutation({
     mutationFn: revertSopVersion,
-    onSuccess: () => {},
-    onError: (error) => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries(["sops"]);
+      setVisibility(false);
+      Toast.show({
+        type: "success",
+        text1: "Success",
+        text2: "SOP Successfully reverted",
+        visibilityTime: 3000,
+      });
+    },
+    onError: (error) => {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: error.message,
+        visibilityTime: 5000,
+      });
+    },
   });
 
   function handleRevert({ id }) {
