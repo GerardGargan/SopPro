f6eac2efd82f01c95813ff7237f8ee34ed8fafef
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2025-03-13 22:17:36 +0000

    Add Setting Model

---
+++ Backend/Backend/Models/DatabaseModels/Setting.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models.DatabaseModels
+{
+    public class Setting : BaseClass
+    {
+        public string Type { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
